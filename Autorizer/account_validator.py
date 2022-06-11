37991db3cbf96f7ee9c7d9853b75fae8b0aa12d2
jesushledon@gmail.com <jesushledon@gmail.com>
2022-06-11 17:43:07 -0500

    A validator for account, use a dict {<validation_method>: <str_error_message>}

--- Autorizer/account_validator.py
+++ Autorizer/account_validator.py
@@ -1,5 +1,25 @@
 from validator import Validator
+from account import Account
 
 class AccountValidator(Validator):
     def __init__(self):
         super().__init__()
+        self.account = None
+        
+    def verify(self):
+        """
+        This method apply a list of verifications to implement
+        and error messages 
+        """
+        validators = {
+            self.exists_a_valid_account: "account-not-initialized"
+        }
+        for validator in validators:
+            if validator():
+                self.violations.append(validators[validator])
+
+    def exists_a_valid_account(self):
+        if self.account:
+            if isinstance(self.account, Account):
+                return True
+        return False
