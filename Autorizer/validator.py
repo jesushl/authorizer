95fc6ae5f39d9d99e379f780e4e8ad3ff7f38c9c
jesushledon@gmail.com <jesushledon@gmail.com>
2022-06-11 13:35:05 -0500

    Adding main validator class

---
+++ Autorizer/validator.py
@@ -0,0 +1,22 @@
+from abc import ABCMeta
+
+class Validator(metaclass=ABCMeta):
+    def __init__(self):
+        """
+        Violations store a list of messages related with 
+        rules violation
+        """
+        self.violations = list()
+
+    def get_status(self) -> list:
+        return self.violations
+
+    def add_violation(self, violation: str):
+        self.violations.append(violation)
+    
+    def verify(self):
+        """
+        All childs of validator should implements a method
+        called verify to create procedures to add violations
+        """
+        pass
