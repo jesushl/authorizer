6fc3d99b994bb86efdf0db5fd6701cda27f04305
jesushledon@gmail.com <jesushledon@gmail.com>
2022-06-23 20:22:05 -0500

    Adding a repr value more defined, also adding a smart way to initialize Account

--- Autorizer/account.py
+++ Autorizer/account.py
@@ -1,9 +1,16 @@
+from os import access
+
 class Account:
-    def __init__(self, active_card=False, available_limit=0):
+    def __init__(self, active_card: bool=None, available_limit: int=None):
         self.active_card = active_card
         self.available_limit = available_limit
         self.violations = []
         self.initialized = False
+        if isinstance(active_card, bool) and isinstance(available_limit, int):
+            self.initialized()
+
+    def initialize(self):
+        self.initialize = True
 
     def add_violations(self, violation:str)->None:
         self.violations.append(violation)
@@ -19,12 +26,12 @@ class Account:
             self.available_limit = self.available_limit - amount
 
     def __repr__(self) -> str:
+        _violations = '"violations": {self.violations}'.format(self=self)
         if self.initialized:
-            _ =  """
-                active: {self.active_card}
-                limit: {self.available_limit}
-                """.format(
-                self=self
-            )
+            _active = '"active-card": {self.active_card}'.format(self=self)
+            _available_limit = '"available-limit": {self.available_limit}'.format(self=self)
+            _account_body = _active +', ' + _available_limit 
         else:
-            _ = '/{/}'
+            _account_body  = '{}'
+        _account = '{"account": {' + _account_body + '}'+ ', ' + _violations + '}' 
+        return _account
